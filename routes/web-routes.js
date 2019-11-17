01da5ffb5bcd47bdcebb138fc611b1efee2d1b53
George Mendoza <george.a.mendoza@gmail.com>
2019-11-17 12:53:23 -0600

    more server fix

--- routes/web-routes.js
+++ routes/web-routes.js
@@ -10,6 +10,10 @@ router.get('/list', (req, res) => {
     res.sendFile('list.html',{root:'./'});
 });
 
+router.get('/list', (req, res) => {
+    res.set('Content-Type','text/html');
+    res.sendFile('list.html',{root:'./'});
+});
 module.exports = router;
 
 
